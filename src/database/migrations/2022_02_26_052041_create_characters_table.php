74cd2ba498cb4970ea6d8282d4221d0c6f64ddb4
octkmr <octkmr@users.noreply.github.com>
2022-02-26 16:42:44 +0900

    tiny fix

--- src/database/migrations/2022_02_26_052041_create_characters_table.php
+++ src/database/migrations/2022_02_26_052041_create_characters_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('characters', function (Blueprint $table) {
             $table->id();
             $table->string('name', 64)->comment('名前');
-            $table->unsignedTinyInteger('unit_id')->comment('部隊ID');
-            $table->date('birthday')->comment('誕生日');
-            $table->unsignedSmallInteger('height')->comment('身長');
+            $table->string('unit')->comment('部隊');
+            $table->string('birthday', 8)->comment('誕生日');
+            $table->unsignedSmallInteger('height')->nullable()->comment('身長');
             $table->string('birthplace', 16)->comment('出身地');
             $table->string('seraph', 64)->comment('セラフ名');
             $table->string('seraphim_code', 128)->comment('セラフィムコード');
