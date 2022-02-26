3eac9ae5d9f270556d3ad2d1e74c052129c8dae0
octkmr <octkmr@users.noreply.github.com>
2022-02-27 08:45:04 +0900

    blade共通化

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>ヘブバン - @yield('title')</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+<footer class="footer">
+</footer>
+</body>
+</html>
